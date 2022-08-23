fa16ad32e4848014783945f2c98f57eeb0972745
Alex <alexonemoreemail@gmail.com>
2022-08-23 16:35:41 +0300

    backup

---
+++ src/atc/parameters.h
@@ -0,0 +1,60 @@
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+#include <QObject>
+#include <QMetaEnum>
+
+namespace ParametersNS {
+	Q_NAMESPACE
+enum class Workmode {
+	SinglePoint,
+	TemperatureRange,
+	CompositionRange,
+	DoubleCompositionRange,
+	TemperatureCompositionRange
+};
+Q_ENUM_NS(Workmode)
+enum class Target {
+	AdiabaticTemperature,
+	Equilibrium
+};
+Q_ENUM_NS(Target)
+enum class LiquidSolution {
+	NoLiquidSolution,
+	OneLiquidSolution
+};
+Q_ENUM_NS(LiquidSolution)
+enum class CompositionUnit {
+	AtomicPercent,
+	WeightPercent,
+	Mol,
+	Gram
+};
+Q_ENUM_NS(CompositionUnit)
+enum class TemperatureUnit {
+	Kelvin,
+	Celsius,
+	Fahrenheit
+};
+Q_ENUM_NS(TemperatureUnit)
+enum class PressureUnit {
+	MPa,
+	atm,
+	bar
+};
+Q_ENUM_NS(PressureUnit)
+}
+
+struct Range {
+	double start, stop, step;
+};
+
+class Parameters
+{
+
+public:
+	Parameters();
+
+};
+
+#endif // PARAMETERS_H
--- src/calculationparameters.cpp
+++ src/calculationparameters.cpp
@@ -1,5 +1,6 @@
 #include "calculationparameters.h"
 #include "ui_calculationparameters.h"
+#include "parameters.h"
 
 CalculationParameters::CalculationParameters(QWidget *parent) :
 	QGroupBox(parent),
@@ -7,6 +8,25 @@ CalculationParameters::CalculationParameters(QWidget *parent) :
 {
 	ui->setupUi(this);
 	setTitle(QStringLiteral("Calculation parameters"));
+
+	auto to_list = [](QMetaEnum&& me) {
+		auto max = me.keyCount();
+		QStringList list;
+		for(int i = 0; i != max; ++i) {
+			auto str = me.valueToKey(me.value(i));
+			list.append(str);
+		}
+		return list;
+	};
+
+	auto workmodes = to_list(QMetaEnum::fromType<ParametersNS::Workmode>());
+	auto targets = to_list(QMetaEnum::fromType<ParametersNS::Target>());
+	auto liquid_solutions = to_list(QMetaEnum::fromType<ParametersNS::LiquidSolution>());
+	auto composition_units = to_list(QMetaEnum::fromType<ParametersNS::CompositionUnit>());
+	auto temperature_units = to_list(QMetaEnum::fromType<ParametersNS::TemperatureUnit>());
+	auto pressure_units = to_list(QMetaEnum::fromType<ParametersNS::PressureUnit>());
+
+
 }
 
 CalculationParameters::~CalculationParameters()
