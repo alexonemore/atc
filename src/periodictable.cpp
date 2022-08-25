c355b088efbd7b21e3b1b609e924e5cae26b36e5
Alex <alexonemoreemail@gmail.com>
2022-08-25 14:31:25 +0300

    backup

--- src/periodictable.cpp
+++ src/periodictable.cpp
@@ -1,11 +1,16 @@
 #include "periodictable.h"
 #include "ui_periodictable.h"
+#include <QDebug>
 
 PeriodicTable::PeriodicTable(QWidget *parent) :
 	QWidget(parent),
 	ui(new Ui::PeriodicTable)
 {
 	ui->setupUi(this);
+
+
+
+
 }
 
 PeriodicTable::~PeriodicTable()
@@ -16,8 +21,12 @@ PeriodicTable::~PeriodicTable()
 QStringList PeriodicTable::GetCheckedElements() const
 {
 	QStringList checked_elements;
-
-
-
+	for(const auto& button : children()) {
+		auto&& p = static_cast<QPushButton*>(button);
+		if(p->isChecked()) {
+			checked_elements.push_back(p->text());
+		}
+	}
+	qDebug() << checked_elements;
 	return checked_elements;
 }
