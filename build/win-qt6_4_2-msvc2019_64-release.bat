e46814e5e2c19dcbc72c4fa56bc301babe250cc4
Alex <alexonemoreemail@gmail.com>
2023-01-19 23:56:53 +0100

    add build 6.4.2

---
+++ build/win-qt6_4_2-msvc2019_64-release.bat
@@ -0,0 +1,8 @@
+set PATH=C:\Qt\6.4.2\msvc2019_64\bin;%PATH%
+mkdir build-win-qt6_4_2-msvc2019_64-release
+cd build-win-qt6_4_2-msvc2019_64-release
+cmake ../../ -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=install/ -D CMAKE_PREFIX_PATH="C:\Qt\6.4.2\msvc2019_64"
+cmake --build . --config Release --parallel --target install
+cd install/bin
+atc.exe
+cd ../../../
