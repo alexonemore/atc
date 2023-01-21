3cb308d4f2846135f93f7548d7653cf0b9330992
Alex <alexonemoreemail@gmail.com>
2023-01-21 14:39:46 +0100

    backup

--- build/linux-release-no-rm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#rm -rf build-linux &&\
-#mkdir build-linux &&\
-cd build-linux &&\
-cmake ../../ -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=install/ &&\
-cmake --build . --config Release --parallel --target install
-cd install/bin
-./atc
--- build/linux-release.sh
+++ build/linux-release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rm -rf build-linux &&\
 mkdir build-linux &&\
 cd build-linux &&\
 cmake ../../ -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=install/ &&\
