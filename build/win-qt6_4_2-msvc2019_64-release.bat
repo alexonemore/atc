50de7782e3037c3c500e1dbaf29a70463056aedd
Alex <alexonemoreemail@gmail.com>
2023-01-23 12:53:59 +0100

    backup

--- build/win-qt6_4_2-msvc2019_64-release.bat
+++ build/win-qt6_4_2-msvc2019_64-release.bat
@@ -4,6 +4,6 @@ cd build-win-qt6_4_2-msvc2019_64-release
 cmake ../../ -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=install/ -D CMAKE_PREFIX_PATH="C:\Qt\6.4.2\msvc2019_64"
 cmake --build . --config Release --parallel 4 --target install
 cd install/bin
-windeployqt atc.exe libqcustomplot.dll
+windeployqt atc.exe qcustomplot.dll
 atc.exe
 cd ../../../
