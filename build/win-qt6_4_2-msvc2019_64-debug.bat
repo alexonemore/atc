c38080975b26b13949d205dfc5e3d15c3702a487
Alex <alexonemoreemail@gmail.com>
2023-01-23 12:49:38 +0100

    fix windeployqt

--- build/win-qt6_4_2-msvc2019_64-debug.bat
+++ build/win-qt6_4_2-msvc2019_64-debug.bat
@@ -2,7 +2,7 @@ set PATH=C:\Qt\6.4.2\msvc2019_64\bin;%PATH%
 mkdir build-win-qt6_4_2-msvc2019_64-debug
 cd build-win-qt6_4_2-msvc2019_64-debug
 cmake ../../ -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=install/ -D CMAKE_PREFIX_PATH="C:\Qt\6.4.2\msvc2019_64"
-cmake --build . --config Debug --parallel --target install
+cmake --build . --config Debug --parallel 4 --target install
 cd install/bin
 atc.exe
 cd ../../../
--- build/win-qt6_4_2-msvc2019_64-release.bat
+++ build/win-qt6_4_2-msvc2019_64-release.bat
@@ -2,7 +2,8 @@ set PATH=C:\Qt\6.4.2\msvc2019_64\bin;%PATH%
 mkdir build-win-qt6_4_2-msvc2019_64-release
 cd build-win-qt6_4_2-msvc2019_64-release
 cmake ../../ -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=install/ -D CMAKE_PREFIX_PATH="C:\Qt\6.4.2\msvc2019_64"
-cmake --build . --config Release --parallel --target install
+cmake --build . --config Release --parallel 4 --target install
 cd install/bin
+windeployqt atc.exe libqcustomplot.dll
 atc.exe
 cd ../../../
